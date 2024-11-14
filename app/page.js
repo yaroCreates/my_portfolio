7600cfcfb0e8eb5540d01c096e9bec9fcb61600d
yaroCreates <onuhjamesyaro@gmail.com>
2024-11-14 10:04:48 +0100

    feat: add header and projects to page layout

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import Hero from "./components/Hero";
+import Projects from "./components/Projects";
 
 export default function Home() {
   return (
     <div className="bg-black">
-      <Hero/>
+      <Hero />
+      <Projects/>
     </div>
   );
 }
