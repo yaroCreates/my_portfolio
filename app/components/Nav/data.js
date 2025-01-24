81e2b73e28d1cac08879e7d5062c4d8485073139
yaroCreates <onuhjamesyaro@gmail.com>
2025-01-24 15:08:36 +0100

    chore: remove contact from nav links

--- app/components/Nav/data.js
+++ app/components/Nav/data.js
@@ -7,10 +7,10 @@ export const links = [
         title: "Projects",
         href: "projects"
     },
-    {
-        title: "Contact",
-        href: "/"
-    }
+    // {
+    //     title: "Contact",
+    //     href: "/"
+    // }
 ]
 
 export const footerLinks = [
